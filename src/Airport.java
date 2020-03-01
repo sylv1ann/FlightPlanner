4ce98b0f1b064f54855841fa57afd42ba20acd85
sylv1ann <viktorbjk@gmail.com>
2020-03-02 00:32:38 +0100

    created package cz.cuni.mff.java.flightplanner

--- src/Airport.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-
-abstract class Airport {
-        static LinkedList<String> runways = new LinkedList<>();
-        static Integer elevation;
-        static Double[] coordinates = new Double[2];
-        static APTCategory cat;
-}
--- src/Corrector.java
+++
@@ -1,4 +0,0 @@
-@FunctionalInterface
-public interface Corrector {
-    String correct();
-}
