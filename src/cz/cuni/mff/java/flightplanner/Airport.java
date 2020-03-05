0d4801c6e1dc5e57f0b687bf8aedd6623f5b93b6
sylv1ann <viktorbjk@gmail.com>
2020-03-05 23:47:36 +0100

    getInput method fixed; several other minor changes

--- src/cz/cuni/mff/java/flightplanner/Airport.java
+++ src/cz/cuni/mff/java/flightplanner/Airport.java
@@ -3,8 +3,8 @@ package cz.cuni.mff.java.flightplanner;
 import java.util.*;
 
 public class Airport {
-        static LinkedList<String> runways = new LinkedList<>();
-        static Integer elevation;
-        static Double[] coordinates = new Double[2];
-        static APTCategory cat;
+    static LinkedList<String> runways = new LinkedList<>();
+    static Integer elevation;
+    static Double[] coordinates = new Double[2];
+    static APTCategory cat;
 }
--- src/cz/cuni/mff/java/flightplanner/DialogCenter.java
+++ src/cz/cuni/mff/java/flightplanner/DialogCenter.java
@@ -1,15 +1,15 @@
 package cz.cuni.mff.java.flightplanner;
 
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
+import org.jetbrains.annotations.*;
 import java.io.*;
 import java.util.*;
+import java.util.stream.Stream;
 
 public class DialogCenter {
 
     public static void main(String[] args) {
         ArrayList<Module> modules, activeModules;
+
         modules = Module.setAllModules(DialogCenter.class.getPackageName());
         showMainMenu();
         activeModules = chooseModules(modules);
@@ -39,14 +39,13 @@ public class DialogCenter {
 
     private static OutputStream setOutputStream() {
         OutputStream result;
-
-        String fileName, filePath;
         System.out.print("Please enter your desired file name: ");
-        fileName = getInput();
+        String fileName = getInput();
         if (!fileName.contains(".txt")) {
             fileName = fileName + ".txt";
         }
         System.out.println("Do you want to choose the directory for your new file? (y/n)\nIf not, current directory will be used.");
+        String filePath;
         if (getInput().toLowerCase().startsWith("y")) {
             System.out.print("Please enter the absolute path of the destination directory: ");
             filePath = getInput();
@@ -67,7 +66,6 @@ public class DialogCenter {
         return result;
     }
 
-
     /**
      * This method only shows initial text information about Flight Planner.
      */
@@ -78,17 +76,16 @@ public class DialogCenter {
 
     private static @NotNull String getInput() {
         String line;
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
             do {
                 line = br.readLine();
             } while (line == null || line.isBlank());
-        }
-        catch (IOException ignored) {
+        } catch (IOException e) {
             System.out.println("Something went wrong. Please try again.");
             line = getInput();
         }
 
-        assert line != null;
         return line;
     }
 
@@ -103,37 +100,43 @@ public class DialogCenter {
         boolean repCond;
         do {
             repCond = true;
-            System.out.println("Please type in all the numbers of options you want to choose (separate the numbers with commas).");
             for (int i = 0; i < modules.size(); i++) {
                 System.out.println("(" + (i + 1) + ") : " + modules.get(i).description);
             }
-            try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+            System.out.print("Please type in all the numbers of options you want to choose (separate the numbers with commas): ");
+            try {
                 line = getInput();
+                System.out.print("\n"); //separates the dialog
                 if (!line.isBlank()) {
                     options = line.split(",");
+                    options = Arrays.stream(options).filter(x -> !x.isBlank()).toArray(String[]::new);
                     for (String opt : options) {
                         try {
-                            if (!opt.isBlank()) {
-                                int x = Integer.parseInt(opt.trim());
-                                if (x <= 0 || x > modules.size())
-                                    throw new WrongInputException("Incorrect option number.\nPlease re-type all your options.");
-                                if (!activeModules.contains(modules.get(x - 1))) {
-                                    activeModules.add(modules.get(x - 1));
-                                }
-                                repCond = false;
+                            int x = Integer.parseInt(opt.trim());
+                            if (x <= 0 || x > modules.size())
+                                throw new WrongInputException("Incorrect option number.\nPlease re-type all your options.");
+                            if (!activeModules.contains(modules.get(x - 1))) {
+                                activeModules.add(modules.get(x - 1));
                             }
+                            repCond = false;
+
                         } catch (NumberFormatException e) {
-                            System.out.println("An error has occured. You typed another character with the number.\nPlease re-type all your options.");
-                            throw new WrongInputException();
+                            Stream<Module> mods;            //this block of code ensures that the program won't fail in case the user types in the part of a module's description.
+                            mods = modules.stream().filter(x -> x.description.toLowerCase().contains(opt.toLowerCase()));
+                            if (mods.count() == 1) {
+                                mods.forEach(activeModules::add); //however, the module has to be unambiguous
+                            } else {
+                                System.out.println("An error has occurred. You typed another character with the number.\nPlease re-type all your options.");
+                                throw new WrongInputException();
+                            }
                         }
                     }
                 }
-            }
-            catch (IOException | WrongInputException e) {
+            } catch (WrongInputException e) {
                 System.out.println("Be sure you choose only correct options " + enumerateModules(modules) + " and separate them with commas.");
                 repCond = true;
             }
-        } while(repCond);
+        } while (repCond);
 
         return activeModules;
     }
@@ -146,7 +149,7 @@ public class DialogCenter {
     @NotNull
     private static String enumerateModules(@NotNull List<Module> modules) {
         StringBuilder result = new StringBuilder("{ ");
-        for(int i = 1; i < modules.size(); i++) {
+        for (int i = 1; i < modules.size(); i++) {
             result.append(i).append(", ");
         }
         result.append(modules.size()).append(" }");
@@ -158,6 +161,7 @@ public class DialogCenter {
      * This method goes through all of the modules and eliminates any incompatible types, such as
      * performs exit procedure if the user wants to exit the Flight Planner or removes all modules if
      * these are included in a more complex module.
+     *
      * @param active = The raw list of modules which have been activated. However, these need to be corrected for incompatible types.
      */
     private static void checkActiveModules(@NotNull ArrayList<Module> active) {
@@ -179,10 +183,14 @@ public class DialogCenter {
                 }
             } catch (InterruptedException | IOException ignored) { }
         }
-        if (active.stream().anyMatch(x -> (x.processNum == 1 || x.processNum == 2)) && (active.stream().anyMatch(x -> x.processNum == 3))) {
+
+        if ((active.stream().anyMatch(x -> x.processNum == 3)) &&
+             active.stream().anyMatch(x -> (x.processNum == 1 || x.processNum == 2))) {
             System.out.println("The most complex operation you want to perform is flight plan creation.\nTherefore, these operations will be suspended: ");
-            active.stream().filter(x -> (x.processNum == 1 || x.processNum == 2)).forEach(x -> { toRemove.add(x);
-                System.out.println("(" + x.processNum + ") : " + x.description);});
+            active.stream().filter(x -> (x.processNum == 1 || x.processNum == 2)).forEach(x -> {
+                toRemove.add(x);
+                System.out.println("(" + x.processNum + ") : " + x.description);
+            });
             active.removeAll(toRemove);
             toRemove.clear();
         }
@@ -192,8 +200,6 @@ public class DialogCenter {
             System.out.println("(" + mod.processNum + ") : " + mod.description);
         }
         System.out.print("\n");
-
-        //return active;
     }
 
 
