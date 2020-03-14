67dc124eb4caf67d623ef90dd83be30e82b3de0b
sylv1ann <viktorbjk@gmail.com>
2020-03-14 19:15:02 +0100

    added underscore to categories

--- src/cz/cuni/mff/java/flightplanner/APTCategory.java
+++ src/cz/cuni/mff/java/flightplanner/APTCategory.java
@@ -4,7 +4,7 @@ package cz.cuni.mff.java.flightplanner;
  * APTCategory enum class only classifies the size of the airport.
  */
 public enum APTCategory {
-    smallAirport,
-    mediumAirport,
-    largeAirport
+    small_airport,
+    medium_airport,
+    large_airport
 }
